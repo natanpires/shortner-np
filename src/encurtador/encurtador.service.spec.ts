a027decfeaad3858013593c514bc3b28be484b4d
Natan Souza <partisansouza@gmail.com>
2021-02-25 10:56:55 -0300

    feat: adjust service unit test.

--- src/encurtador/encurtador.service.spec.ts
+++ src/encurtador/encurtador.service.spec.ts
@@ -2,17 +2,18 @@ import { Test } from '@nestjs/testing';
 import { EncurtadorService } from './encurtador.service';
 import { Encurtador } from './encurtador.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { ShowDto } from './dto/show.dto';
 
-describe('EncurtadorController', () => {
+describe('EncurtadorService', () => {
   let service: EncurtadorService;
 
   const mockRepository = {
     create: jest.fn().mockReturnValue({
       url: 'http://wisereducacao.com',
-      newUrl: 'localhost:8081/ZTafrj',
+      newUrl: 'localhost:8081/undefined',
     }),
-    find: jest.fn().mockReturnValue({
-      url: 'http://teste.com',
+    findOne: jest.fn().mockReturnValue({
+      url: 'http://wisereducacao.com',
     }),
   };
 
@@ -30,39 +31,30 @@ describe('EncurtadorController', () => {
     done();
   });
 
-  describe('EncurtadorService', () => {
+  describe('Service', () => {
     it('Should be defined', () => {
       expect(service).toBeDefined();
     });
 
     it('should create an ShowDto object', async (done) => {
-      try {
-        const obj = {
-          longUrl: 'http://wisereducacao.com',
-        };
-        const data = service.create(obj);
-        expect(data).resolves.toBe({
-          url: 'http://wisereducacao.com',
-          newUrl: 'localhost:8081/ZTafrj',
-        });
-        done();
-      } catch (e) {
-        console.error(e);
-        done();
-      }
+      const result = new ShowDto();
+      result.newUrl = 'localhost:8081/undefined';
+      result.url = 'http://wisereducacao.com';
+
+      const data = service.create({
+        longUrl: 'http://wisereducacao.com',
+      });
+      expect(data).resolves.toStrictEqual(result);
+      done();
     });
 
     it('should find and return an ShowDto object', async (done) => {
-      try {
-        const data = service.find('lyQUgJ');
-        expect(data).resolves.toBe({
-          url: 'http://teste.com',
-        });
-        done();
-      } catch (e) {
-        console.error(e);
-        done();
-      }
+      const result = new ShowDto();
+      result.url = 'http://wisereducacao.com';
+
+      const data = service.find('lyQUgJ');
+      expect(data).resolves.toStrictEqual(result);
+      done();
     });
   });
 });
